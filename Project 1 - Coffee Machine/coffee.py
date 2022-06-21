980a2cd8ce8676171faf9a29fe9eabc4c7383c14
MarinheiroB <marinheiro.barbara@gmail.com>
2022-06-21 08:28:01 -0300

    Reorganize

---
+++ Project 1 - Coffee Machine/coffee.py
@@ -0,0 +1,62 @@
+from ctypes import sizeof
+from mimetypes import init
+import time
+from tqdm import tqdm
+
+DIVIDER = "================================================="
+
+
+class Coffee():
+    def __init__(self, name, price, size, ingredients):
+        self.name = name
+        self.price = price
+        self.size = size
+        self.ingredients = ingredients
+
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
+
+    def print_ingredients(self):
+        milk = self.ingredients['milk']
+        water = self.ingredients['water']
+        coffee = self.ingredients['coffee']
+        string = ''
+        if(milk != 0):
+            string += f'Leite: {milk} ml    '
+        if(water != 0):
+            string += f'Água: {water} ml    '
+        if(coffee != 0):
+            string += f'Café: {coffee} g'
+
+        print(DIVIDER)
+        print(string)
+        print(DIVIDER)
+
+    def get_ingredient(self, ingredient_name):
+        return self.ingredients.get(ingredient_name, "Ingrediente não encontrado!")
+
+    def print_coffee(self):
+        print(DIVIDER)
+        print("           **Preparando a bebida**               ")
+        print(DIVIDER)
+        for i in tqdm(range(100), desc=False, ascii=" ♥", ncols=50):
+            time.sleep(0.02)
+        print(DIVIDER)
+        print("               **Bebida pronta**                 ")
+        print(DIVIDER)
+        print("                 }___{ ___{                      ")
+        print("              .-{    }     }-.                   ")
+        print("             (   }        {   )                  ")
+        print("             |`-.________..-'|                   ")
+        print("             |                ;--.               ")
+        print("             |               (__  \              ")
+        print("             |                | )  )             ")
+        print("             |                |/  /              ")
+        print("             |                /  /               ")
+        print("             |               (  /                ")
+        print("             \                y'                 ")
+        print("              `-..________..-'                   ")
+        print(DIVIDER)
